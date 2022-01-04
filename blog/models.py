ad2e82fe58f36b780e5c406c6ddd9ed27224e5dc
shunna87 <anush_av@instigatemobile.com>
2022-01-04 15:28:40 +0400

    first commit

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length= 255)
+    author = models.ForeignKey(User, on_delete= models.CASCADE)
+    body = models.TextField(max_length= 70000)
+
+    def __str__(self):
+        return self.title
+
+# Create your models here.
---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from . import views
+from .views import Post
+from .views import HomeView, ArticleDetailView
+
+urlpatterns = [
+   path('', HomeView.as_view(), name = "home"),
+   path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
+
+]
