c3286318196a5b2b7ea0ee523b8ab7264beff98d
shunna87 <anush_av@instigatemobile.com>
2022-01-06 18:34:38 +0400

    design updating

--- Chinablog/settings.py
+++ Chinablog/settings.py
@@ -34,9 +34,10 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+
     'blog',
     'crispy_forms',
     'ckeditor',
