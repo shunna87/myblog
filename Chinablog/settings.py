23d880f24bf891a3906e5449b8e23289e9ad8d02
shunna87 <anush_av@instigatemobile.com>
2022-01-09 20:30:22 +0400

    debug=true

--- Chinablog/settings.py
+++ Chinablog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v#rz%3so4(uvf3szfen4&t^z0_=l6&6+fxb)_$o1gb+@+=phuc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
